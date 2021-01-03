ec27ba9baad9cc01e04a3ca88bbe07c1bec9096b
mateofrito <mattfry@outlook.com>
2021-01-02 22:09:30 -0500

    start of converting dos prompt to Blazor

---
+++ BlazorDOS/PriorInputs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDOS
+{
+    public class PriorInputs
+    {
+        public string PriorInput { get; set; }
+        
+
+    }
+}
