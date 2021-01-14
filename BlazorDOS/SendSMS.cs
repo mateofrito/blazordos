ce04418fbf58326314276226a6009f1780c82f09
mateofrito <mattfry@outlook.com>
2021-01-13 19:59:12 -0500

    added environment variables

--- BlazorDOS/SendSMS.cs
+++ BlazorDOS/SendSMS.cs
@@ -15,8 +15,8 @@ namespace BlazorDOS
         {
             // Find your Account Sid and Token at twilio.com/console
             // and set the environment variables. See http://twil.io/secure
-            string accountSid = "ACa6c79dc7bf6ddbb56132219f24f3346b";
-            string authToken = "49d494df7513bebac55ac9d0842d0c97";
+            string accountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+            string authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
             string drewNumber = "13308012990";
             string fryNumber = "16142149306";
             TwilioClient.Init(accountSid, authToken);
--- BlazorDOS/Startup.cs
+++ BlazorDOS/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using BlazorDOS.Data;
+using BlazorDOS.Commands;
 
 namespace BlazorDOS
 {
@@ -29,7 +30,8 @@ namespace BlazorDOS
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
-            services.AddSingleton<SendSMS>(); 
+            services.AddSingleton<SendSMS>();
+            services.AddSingleton<BaseCommands>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
